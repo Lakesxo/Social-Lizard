490efe33f6c6a3b177b364f6da2f45763a0729cb
unknown <lakes.ajanaku@gmail.com>
2021-10-31 16:53:45 +0100

    updated contact section

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import Heading from '../components/Heading'
 import SocialIcons from '../components/SocialIcons'
 import { useForm } from '@formspree/react';
 
 const Contact = () => {
     const [state, handleSubmit] = useForm("xvodjgjo");
-    if (state.succeeded) {
-        console.log('sent boss')
-    }
+    const [deliveredForm, setDeliveredForm] = useState(false)
+    const formRef = useRef()
+
+    useEffect(() => {
+        if (state.succeeded) {
+            setDeliveredForm(true)
+            setTimeout(() => setDeliveredForm(false), 5000)
+        }
+        formRef.current.reset()
+    }, [state.succeeded])
  
     return (
         <div className="contactCont" id="contact">
@@ -15,13 +22,15 @@ const Contact = () => {
             <div className="midTxt">
                 <p className="catchy">Here’s where you’d insert a catchy phrase for the contact us section</p>
             </div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} ref={formRef}>
                 <div className="flexInp">
-                    <input type="text" name="name" id="name" placeholder="Name" />
-                    <input type="email" name="email" id="email" placeholder="Email" />
+                    <input type="text" name="name" id="name" placeholder="Name" required />
+                    <input type="email" name="email" id="email" placeholder="Email" required />
                 </div>
                 <textarea placeholder="Message" name="message" id="message" cols="30" rows="10"></textarea>
-                <button disabled={state.submitting} className="start fullWdth">Send</button>
+                {
+                    deliveredForm ? <p className="thanks">Thanks for reaching out !</p> : <button disabled={state.submitting} className="start fullWdth">Send</button>
+                }
             </form>
             <div className="centSc">
                 <SocialIcons />
