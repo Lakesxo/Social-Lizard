2e0d3db49f3d271d43122b0bab612e1170b4c031
unknown <lakes.ajanaku@gmail.com>
2022-01-30 17:41:46 +0100

    updates

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -31,7 +31,7 @@ const Hero = () => {
                     </div>
                     <div className="rightHero">
                         <img draggable="false" src="https://res.cloudinary.com/dt9pwfpi5/image/upload/v1630147706/Social_tree_uoojjt.gif" alt="social lizard" />
-                        <img className="path" data-aos="fade-left" draggable="false" src={isMobile ? "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1641982519/Group_mb_bi0s7l.png" : "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1643554803/lastbg_kntg3a.png"} alt="social lizard" />
+                        <img className="path" data-aos="fade-left" draggable="false" src={isMobile ? "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1641982519/Group_mb_bi0s7l.png" : "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1643560025/newbg_a3ghjb.png"} alt="social lizard" />
                     </div>
                 </div>
                 <img draggable='false' src={isMobile ? "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1630503716/Ellipse_3_rnwuq7.png" : "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1630235221/Ellipse_1_u8lyv6.png"} alt="social lizard" className="bgImg" />
