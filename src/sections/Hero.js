bd2bbfa79da5e25d583896977f9baa07e4c26abe
unknown <lakes.ajanaku@gmail.com>
2022-01-30 16:01:30 +0100

    changes

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -24,13 +24,14 @@ const Hero = () => {
                 <div className="flexHero">
                     <div className="leftHero">
                         <div className="textnBtn">
-                            <p className="heroDeets">Grow <span className="extra">your</span> creative business <span className="extra">with</span> social <span className="extra">lizard</span>.</p>
+                            <p className="heroDeets">GROW <span className="extra">YOUR</span><br /> CREATIVE BUSINESS <span className="extra">WITH</span><br /> SOCIAL LIZARD</p>
                             <Link to="services" spy={true}><button className="start">Get started</button></Link>
                         </div>
-                        <img className="path" data-aos="fade-left" draggable="false" src={isMobile ? "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1641982519/Group_mb_bi0s7l.png" : "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1641982519/Group_desk_gqoxvz.png"} alt="social lizard" />
+                        {/* <img className="path" data-aos="fade-left" draggable="false" src={isMobile ? "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1641982519/Group_mb_bi0s7l.png" : "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1641982519/Group_desk_gqoxvz.png"} alt="social lizard" /> */}
                     </div>
                     <div className="rightHero">
                         <img draggable="false" src="https://res.cloudinary.com/dt9pwfpi5/image/upload/v1630147706/Social_tree_uoojjt.gif" alt="social lizard" />
+                        <img className="path" data-aos="fade-left" draggable="false" src={isMobile ? "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1641982519/Group_mb_bi0s7l.png" : "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1643554803/lastbg_kntg3a.png"} alt="social lizard" />
                     </div>
                 </div>
                 <img draggable='false' src={isMobile ? "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1630503716/Ellipse_3_rnwuq7.png" : "https://res.cloudinary.com/dt9pwfpi5/image/upload/v1630235221/Ellipse_1_u8lyv6.png"} alt="social lizard" className="bgImg" />
